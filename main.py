12a205b72fc2ebb0b354025078aa37afbfede7a7
= <hellospeakman@gmail.com>
2021-10-26 15:01:01 +0100

    Added a 24 round system and end game screen. Registering clicks on the colours. Added round counter for when the shapes and winning shape are the same as previous.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import sys
 import pygame
 import factory
-from shape import Rectangle, Circle, Triangle, Parallelogram
+from shape import Rectangle, Circle, Triangle, Parallelogram, Shape
 
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 
@@ -21,8 +21,16 @@ def load_shapes():
     return [factory.create(item) for item in data["shapes"]]
 
 
-def draw():
-    clear_drawings = pygame.draw.rect(SCREEN, (0, 0, 0), (0, 0, 800, 600))
+def draw_end_game():
+    pygame.draw.rect(SCREEN, (0, 0, 0), (0, 0, 800, 600))
+    font = pygame.font.Font(None, 48)
+    text = font.render("Thank you for your time.", True, (255, 255, 255))
+    text_rect = text.get_rect(center=(400, 300))
+    SCREEN.blit(text, text_rect)
+
+
+def draw() -> Shape:
+    pygame.draw.rect(SCREEN, (0, 0, 0), (0, 0, 800, 600))
 
     random.shuffle(shapes)
     for index, shape in enumerate(shapes):
@@ -37,12 +45,17 @@ def draw():
         elif shape.method == 'polygon':
             pygame.draw.polygon(SCREEN, shape.colour, position)
 
-    winning_shape = shapes[0]
+    target_shape = shapes[random.randint(0, 3)]
+    if "last_shape" in locals():
+        while target_shape.method == last_shape:
+            target_shape = shapes[random.randint(0, 3)]
 
     font = pygame.font.Font(None, 48)
-    text = font.render(f"Please select the {winning_shape.type}", True, (255, 255, 255))
+    text = font.render(f"{current_round}. Please select the {target_shape.type}", True, (255, 255, 255))
     text_rect = text.get_rect(center=(400, 50))
     SCREEN.blit(text, text_rect)
+
+    return target_shape
 
 
 shapes = load_shapes()
@@ -55,14 +68,29 @@ SCREEN = pygame.display.set_mode((800, 600))
 generate_new_round = pygame.USEREVENT + 1
 pygame.time.set_timer(generate_new_round, 5000)
 
-draw()
+max_rounds = 24
+current_round = 1
+
+winning_shape = draw()
+last_shape = winning_shape.type
+
 while True:
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-        if events.type == generate_new_round:
-            draw()
+        if events.type == generate_new_round and current_round <= 24:
+            current_round = current_round + 1
+
+            if current_round > 24:
+                draw_end_game()
+            else:
+                winning_shape = draw()
+
+        if events.type == pygame.MOUSEBUTTONDOWN and current_round <= 24:
+            clicked = SCREEN.get_at(pygame.mouse.get_pos())
+            if clicked == winning_shape.colour:
+                print(f"You clicked the {winning_shape.type} successfully.")
 
     pygame.display.update()
