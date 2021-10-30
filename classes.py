1e551403a576165711a66c829e3f8a14cac7d52f
Pedro <p.ribeiro.engenheiro@gmail.com>
2021-10-31 00:32:00 +0100

    Added classes subject, with handler, timestamp, with handler, and testdata, and handler. these are meant to identify test subject and input timestamps of the program phase into the test data, respectively, the test class is meant to compile this data into a single dict and then a json. to main added functions for second and third rounds, second meant to be a spinoof of first in which only one shape spawns in a random position, third phase is the showing of images pulled from folder IAPS, intercalated by whitespaces, defined by function whitespaces. Included all these rounds in pygame loop.

---
+++ classes.py
@@ -0,0 +1,82 @@
+import datetime
+from dataclasses import dataclass
+# from mpydev import BioPac as bp
+import numpy as np
+from datetime import date
+
+
+@dataclass
+class Subject:
+    Subject_ID: int
+    Subject_ID = '0'
+    Subject_Test_Date = date.today()
+    Subject_Key = f'{Subject_ID}({Subject_Test_Date})'
+
+
+class SubjectHandle(Subject):
+    def identify_subject(self):
+        subject_id = input('Insert subject ID')
+        if isinstance(subject_id, int):
+            self.Subject_ID = subject_id
+        else:
+            raise ValueError('The Subjects ID must be an integer')
+            self.identify_subject()
+
+    def get_subject_id(self):
+        return self.Subject_ID
+
+    def get_subject(self):
+        return self.Subject_Key
+
+
+@dataclass
+class TestData(SubjectHandle):
+    Test_Data: dict
+
+
+class TestDataHandler(TestData):
+
+    def initialize_test(self):
+        self.identify_subject()
+        key = self.Subject_Key()
+        self.Test_Data = {key: {}}
+
+    def first_timestamp(self, first_timestamp_list: dict):
+        self.Test_Data[self.get_subject()]['Timestamps'] = first_timestamp_list
+
+    def timestamp_add(self, timestamp: dict):
+        self.Test_Data[self.get_subject()]['Timestamps'].update(timestamp)
+
+    def database_commit(self):
+        with open(f'{self.get_subject()}.txt') as json:
+            json.dump(self.Test_Data)
+
+
+@dataclass
+class TimeStamps(TestDataHandler):
+    timestamp: int or float or str
+    ts_dict: dict
+    stage: str
+    one_to_twenty_four: list
+    one_to_twenty: list
+    one_to_twenty_four = np.arange(1, 25, 1)
+    twenty_seven_to_eighty_one = np.arange(27, 81, 1)
+    allowed_stages = ['Baseline', f'Q{one_to_twenty_four}', f'IAPS{twenty_seven_to_eighty_one}']
+
+
+class ManageTimestamps(TimeStamps):
+    def create_timestamp(self, stage: str):
+        if stage in self.allowed_stages:
+            self.stage = stage
+        else:
+            raise ValueError('Stage must be one of the pre-approved for this protocol.')
+            return
+        # timestamp = bp.get_timestamp()
+        timestamp = 1818
+        self.timestamp = timestamp
+        self.ts_dict = {self.stage: self.timestamp}
+        if self.stage == 'Baseline':
+            self.first_timestamp(self.ts_dict)
+        else:
+            self.timestamp_add(self.ts_dict)
+        return self.ts_dict
--- main.py
+++ main.py
@@ -6,8 +6,25 @@ import pygame
 import factory
 import loader
 from shape import Rectangle, Circle, Triangle, Parallelogram, Shape
+from classes import ManageTimestamps
+from PIL import Image
+import glob
+import time
+
+print('1')
 
 os.environ["SDL_VIDEO_CENTERED"] = "1"
+
+print('2')
+
+picked_IAPS = []
+
+print('3')
+
+data_management = ManageTimestamps
+data_management.initialize_test()
+
+print('4')
 
 
 def load_shapes():
@@ -18,7 +35,7 @@ def load_shapes():
     factory.register("triangle", Triangle)
     factory.register("parallelogram", Parallelogram)
 
-    with open('./shapes.json') as file:
+    with open('shapes.json') as file:
         data = json.load(file)
         loader.load_plugins(data["plugins"])
 
@@ -69,22 +86,90 @@ def draw() -> Shape:
     return target_shape
 
 
+def draw_second_stage() -> Shape:
+    """Draw the next round screen and return the chosen shape"""
+    pygame.draw.rect(SCREEN, (0, 0, 0), (0, 0, 800, 600))
+
+    random.shuffle(shapes)
+    for index, shape in enumerate(shapes):
+        if index > 0:
+            break
+
+        position = shape.map(positions[index])
+
+        # TODO: Fix the issue with pygame and being able to draw the shape from the shape class
+        # TODO: This is a dirty hack because of the 24 hour deadline from start to finish
+        if shape.method == 'rect':
+            pygame.draw.rect(SCREEN, shape.colour, position)
+        elif shape.method == 'circle':
+            pygame.draw.circle(SCREEN, shape.colour, position, shape.radius)
+        elif shape.method == 'polygon':
+            pygame.draw.polygon(SCREEN, shape.colour, position)
+        elif shape.method == 'line':
+            pygame.draw.line(SCREEN, shape.colour, position[0], position[1], shape.radius)
+
+    target_shape = shapes[random.randint(0, 3)]
+    while target_shape.method == last_shape:
+        target_shape = shapes[random.randint(0, 3)]
+
+    font = pygame.font.Font(None, 48)
+    text = font.render(f"{current_round}. Please select the {target_shape.type}", True, (255, 255, 255))
+    text_rect = text.get_rect(center=(400, 50))
+    SCREEN.blit(text, text_rect)
+
+    return target_shape
+
+
+def draw_third_stage() -> Shape:
+    """Draw the next round screen and return the chosen shape"""
+    pygame.draw.rect(SCREEN, (0, 0, 0), (0, 0, 800, 600))
+    path = 'D:\pedro\Documents\Tese\data_collect_py\IAPS/*.jpg'
+    IAPS = []
+    for filename in glob.glob(path):
+        IAPS.append(filename)
+    figure = random.choice(IAPS)
+    if figure in picked_IAPS:
+        while figure in picked_IAPS:
+            figure = random.choice(IAPS)
+    image = pygame.image.load(figure)
+    SCREEN.fill(0, 0, 0)
+    SCREEN.blit(image, (0, 0))
+    return figure
+
+
+def draw_whitespace():
+    """Draw the next round screen and return the chosen shape"""
+    pygame.draw.rect(SCREEN, (0, 0, 0), (0, 0, 800, 600))
+    SCREEN.fill(255, 255, 255)
+
+
+print('5')
+
 shapes = load_shapes()
 positions = ["top_left", "top_right", "bottom_left", "bottom_right"]
 
+print('6')
+
 pygame.init()
 SCREEN = pygame.display.set_mode((800, 600))
+
+print('7')
 
 # TODO: Build structure / classes to handle events better
 generate_new_round = pygame.USEREVENT + 1
 pygame.time.set_timer(generate_new_round, 5000)
 
-max_rounds = 24
-current_round = 1
+print('8')
 
+max_rounds = 81
+current_round = 1
+whitespace_rounds = [25, 26, 28, 29, 31, 32, 34, 35, 37, 38, 40, 41, 43, 44, 46, 47, 49, 50, 52, 53, 55, 56, 58, 59, 61,
+                     62, 64, 65, 67, 68, 70, 71, 73, 74, 76, 77, 79, 80]
 last_shape = None
 winning_shape = draw()
 clickable = True
+
+print('9')
 
 while True:
     for events in pygame.event.get():
@@ -95,17 +180,43 @@ while True:
         if events.type == generate_new_round and current_round <= 24:
             current_round = current_round + 1
 
-            if current_round > 24:
+            if current_round > 44:
+                data_management.database_commit()
                 draw_end_game()
-            else:
+            if current_round < 12:
                 clickable = True
                 winning_shape = draw()
                 last_shape = winning_shape.type
+                continue
+            if 12 < current_round < 24:
+                clickable = True
+                winning_shape = draw_second_stage()
+                last_shape = winning_shape.type
+                continue
 
         if events.type == pygame.MOUSEBUTTONDOWN and current_round <= 24 and clickable is True:
             clicked = SCREEN.get_at(pygame.mouse.get_pos())
             if clicked == winning_shape.colour:
                 clickable = False
                 print(f"You clicked the {winning_shape.type} successfully.")
+                if current_round == 1:
+                    stage = 'Baseline'
+                    data_management.create_timestamp(stage)
 
+                else:
+                    stage = f'Q{current_round}'
+                    data_management.create_timestamp(stage)
+        if 24 < current_round < 44:
+            if current_round not in whitespace_rounds:
+                file = draw_third_stage()
+                stage = f'IAPS{current_round}'
+                data_management.create_timestamp(stage)
+                continue
+            if current_round in whitespace_rounds:
+                stage = 'Baseline'
+                data_management.create_timestamp(stage)
+                draw_whitespace()
+                continue
     pygame.display.update()
+
+print('10')
