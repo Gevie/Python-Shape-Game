63f94a88cdcb75a6603159c693bf549b3618f1a0
= <hellospeakman@gmail.com>
2021-10-31 05:01:06 +0000

    Building out the class structure for the game, structure and rounds. Planning ahead.

--- subject.py
+++ subject.py
@@ -8,8 +8,10 @@ from typing import List
 class Round:
     allowed_stages: List = field(default_factory=lambda: ['Game', 'SingleChoiceGame', 'Stimulation'])
     current_round: int = 1
+    iap: int = 0
     max_rounds: int = 12
     stage: str = 'Game'
+    stimulation_type: str = 'Baseline'
 
     def increment(self) -> None:
         if self.max_rounds <= self.current_round:
@@ -24,6 +26,9 @@ class Round:
 
         self.stage = stage
 
+    def switch_stimulation_type(self) -> None:
+        self.stimulation_type = f'IAPS{self.iap}' if self.stimulation_type == 'Baseline' else 'Baseline'
+
 
 @dataclass
 class Subject:
@@ -31,17 +36,29 @@ class Subject:
     date: date
     data: dict
 
-    def get_id(self):
-        pass
-
-    def get_date(self):
-        pass
-
     def get_key(self):
+        """Questioning why this is needed given the data example Pedro gave me"""
         pass
 
-    def add_timestamp(self, current_round: Round, timestamp: str):
-        pass
+    def create_data_template(self):
+        self.data = {
+            self.id: {
+                self.date: {
+                    'Timestamps': {
+                        'Baseline': {}
+                    },
+                    'Stimulation': {}
+                }
+            }
+        }
+
+    def add_timestamp(self, current_round: Round, timestamp: str) -> None:
+        if current_round.stage != 'Stimulation':
+            current_round = f'Q{current_round.current_round}'
+            self.data[self.id][self.date]['Timestamps']['Baseline'].update({current_round: timestamp})
+            return None
+
+        self.data[self.id][self.date]['Stimulation'][current_round.stimulation_type] = timestamp
 
     def save(self):
         pass
@@ -56,6 +73,11 @@ class Instance:
     def add_timestamp(self) -> None:
         timestamp = self.recording_handler.get_timestamp()
         self.subject.add_timestamp(self.current_round, timestamp)
+
+        if self.current_round.stage == 'Stimulation':
+            self.current_round.switch_stimulation_type()
+
+        self.current_round.increment()  # TODO: Move this elsewhere
 
     def save_results(self):
         self.recording_handler.tear_down()
