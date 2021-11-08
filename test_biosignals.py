afaf92922e0bd96618d3e8afbb7b382a063ee78f
pedrorribeiro <68255410+pedrorribeiro@users.noreply.github.com>
2021-11-08 11:53:10 +0000

    added test_biosignals.py

--- test_biosignals.py
+++ test_biosignals.py
@@ -10,3 +10,19 @@ class TestBioSignals(unittest.TestCase):
         self.assertIsNotNone(Device.source)
         Device.set_port(5)
         self.assertIsNotNone(Device.source)
+
+    def test_prepare_dict(self):
+        Session.prepare_dict()
+        self.assertIsNotNone(Session.session_data)
+
+    def test_update_dict(self):
+        Session.update_dict(1, 'r')
+
+    def test_eeg_start(self):
+        Recording.eeg_start()
+
+    def test_eeg_stop(self):
+        Recording.eeg_stop()
+
+if __name__ == '__main__':
+    unittest.main
