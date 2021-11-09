7e6f5c1719c67edc25a0c4393fdfd0606aec41e2
pedrorribeiro <68255410+pedrorribeiro@users.noreply.github.com>
2021-11-09 14:44:31 +0000

    Initialized all necessary classes, prepared for the short signal recording session

--- pretest.py
+++ pretest.py
@@ -1,4 +1,19 @@
-# TODO: initialize all necessary classes
+from biosignals import Device, Session, Recording
+from recording import BioPlusRecording
+import time
+
 # TODO: prepare to open log files for recordings
 # TODO: prepare to plot all necessary data
+
+'''
+Initializing all of the necessary classes
+'''
+eeg_device = Device(1)
+eeg_session = Session(1200, {}, 16, field(default_factory=lambda: [8, 16]), field(default_factory=lambda: ['fs', 'res',
+                                                                                                           'source']))
+eeg_session.prepare_dict()
+eeg = Recording(eeg_session)
+BioPlusRecording = BioPlusRecording()
+time.sleep(20)
+BioPlusRecording.tear_down()
 
